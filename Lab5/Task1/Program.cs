90236664d716ea97bc7afc25fd411e5e63c6d1d1
SinkoSevastian <sinko.sevastian1122@vu.cdu.edu.ua>
2024-01-22 07:41:53 -0700

    first commit

---
+++ Lab5/Task1/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Task1
+{
+    struct MyFrac
+    {
+        public long nom, denom;
+        public MyFrac(long nom_, long denom_)
+        {
+            nom = nom_;
+            denom = denom_;
+            if ((denom < 0 && nom >= 0) || (denom < 0 && nom < 0))
+            {
+                denom *= -1;
+                nom *= -1;
+            }
+            
+            long a = Math.Abs(nom);
+            long b = Math.Abs(denom);
+            while (b != 0)
+            {
+                long temp = b;
+                b = a % b;
+                a = temp;
+            }
+            nom /= a;
+            denom /= a;
+        }
+
+        public override string ToString() => denom != 1 ? $"{nom}/{denom}" : $"{nom}";
+    }
+    internal class Program
+    {
+        static string ToStringWithIntegerPart(MyFrac f)
+        {
+            var nom = f.nom;
+            var denom = f.denom;
+            if (Math.Abs(nom) > Math.Abs(denom))
+            {
+                return nom < 0 ? $"-({Math.Abs(nom / denom)} + {Math.Abs(nom % denom)}/{denom})" : $"{nom / denom} + {nom % denom}/{denom}";
+            }
+            else
+            {
+                return $"{nom}/{denom}";
+            }
+        }
+
+        static double DoubleValue(MyFrac f)
+        {
+            return Convert.ToDouble(f.nom) / Convert.ToDouble(f.denom);
+        }
+
+        static MyFrac Plus(MyFrac f1, MyFrac f2)
+        {
+            return new MyFrac(f1.nom * f2.denom + f2.nom * f1.denom, f1.denom * f2.denom);
+        }
+        static MyFrac Minus(MyFrac f1, MyFrac f2)
+        {
+            return new MyFrac(f1.nom * f2.denom - f2.nom * f1.denom, f1.denom * f2.denom);
+        }
+
+        static MyFrac Multiply(MyFrac f1, MyFrac f2)
+        {
+            return new MyFrac(f1.nom * f2.nom, f1.denom * f2.denom);
+        }
+        static MyFrac Divide(MyFrac f1, MyFrac f2)
+        {
+            return new MyFrac(f1.nom * f2.denom, f1.denom * f2.nom);
+        }
+
+        static MyFrac CalcSum1(int n)
+        {
+            MyFrac first = new MyFrac(1, 2);
+            for (int j = 2; j <= n; j++)
+            {
+                MyFrac neu = new MyFrac(1, j * (j + 1));
+                first = Plus(first, neu);
+            }
+            return first;
+        }
+        static MyFrac CalcSum2(int n)
+        {
+            MyFrac first = new MyFrac(3, 4);
+            for (int j = 3; j <= n; j++)
+            {
+                MyFrac neu = new MyFrac(j * j - 1, j * j);
+                first = Multiply(first, neu);
+            }
+            return first;
+        }
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            MyFrac res_1 = new MyFrac(-4, -18);
+            MyFrac res_2 = new MyFrac(4, 18);
+            Console.WriteLine(res_1);
+            Console.WriteLine(ToStringWithIntegerPart(res_1));
+            Console.WriteLine($"Результат: {DoubleValue(res_1)}");
+            Console.WriteLine($"Результат додавання двох дробів: {Plus(res_1, res_2)}");
+            Console.WriteLine($"Результат віднімання двох дробів:{Minus(res_1, res_2)}");
+            Console.WriteLine($"Результат множення двох дробів: {Multiply(res_1, res_2)}");
+            Console.WriteLine($"Результат ділення двох дробів: {Divide(res_1, res_2)}");
+            Console.WriteLine($"{CalcSum1(6)}");
+            Console.WriteLine($"{CalcSum2(8)}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab5/Task2/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.Security;
+
+namespace struct_lab_student
+{
+    struct Student
+    {
+        public string surName;
+        public string firstName;
+        public string patronymic;
+        public char sex;
+        public string dateOfBirth;
+        public char mathematicsMark;
+        public char physicsMark;
+        public char informaticsMark;
+        public int scholarship;
+
+        public Student(string lineWithAllData)
+        {
+            string[] student = Regex.Split(lineWithAllData, @" ");
+            surName = student[0];
+            firstName = student[1];
+            patronymic = student[2];
+            sex = Convert.ToChar(student[3]);
+            dateOfBirth = student[4];
+            mathematicsMark = Convert.ToChar(student[5]);
+            physicsMark = Convert.ToChar(student[6]);
+            informaticsMark = Convert.ToChar(student[7]);
+            scholarship = Convert.ToInt32(student[8]);
+
+        }
+        public override string ToString()
+        {
+            return $"{surName} {firstName} {patronymic} {sex} {dateOfBirth} {mathematicsMark} {physicsMark} {informaticsMark} {scholarship}";
+        }
+    }
+    internal class Program
+    {
+        public static List<Student> ReadData()
+        {
+            FileStream fs = File.Open(@"data.txt", FileMode.Open);
+            List<Student> students = new List<Student>();
+            using (var reader = new StreamReader(fs))
+            {
+                while (reader.Peek() >= 0)
+                {
+                    string str = reader.ReadLine() ?? string.Empty;
+                    Student student = new Student(str);
+                    students.Add(student);
+                }
+            }
+            return students;
+        }
+
+        static double GetSummarySerArifm(List<Student> allStudents)
+        {
+            double summary_arifm = 0;
+
+            for (int i = 0; i < allStudents.Count; i++)
+            {
+                Student student = allStudents[i];
+                if (student.mathematicsMark == '-')
+                {
+                    student.mathematicsMark = '2';
+                }
+                if (student.physicsMark == '-')
+                {
+                    student.physicsMark = '2';
+                }
+                if (student.informaticsMark == '-')
+                {
+                    student.informaticsMark = '2';
+                }
+
+                summary_arifm += GetArifm(student);
+            }
+            return summary_arifm / allStudents.Count;
+        }
+
+        static double GetArifm(Student student)
+        {
+            double math = Char.GetNumericValue(student.mathematicsMark);
+            double physics = Char.GetNumericValue(student.physicsMark);
+            double it = Char.GetNumericValue(student.informaticsMark);
+
+            return (math + physics + it)/3.0;
+        }
+        public static List<Student> SerArifmetic_14var(List<Student> allStudents)
+        {
+            List<Student> result = new List<Student>();
+
+            double summary_arifm = GetSummarySerArifm(allStudents);
+
+            Console.WriteLine("Загальнє середнє арифметичне: {0}", summary_arifm);
+            foreach(Student student in allStudents)
+            {
+                if(GetArifm(student)>=summary_arifm)
+                {
+                    result.Add(student);
+                    Console.WriteLine(student.surName);
+                }
+            }           
+            return result;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            List<Student> students = ReadData();
+
+            SerArifmetic_14var(students);
+        }
+    }
+}
